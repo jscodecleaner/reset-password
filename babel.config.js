09f98254660756395574296d59fb100e075de291
james2-at-492338268766 <settingsalex@gmail.com>
2023-05-08 06:11:41 -0400

    fix issues for unit test

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,17 @@
 module.exports = {
-  presets: ['module:metro-react-native-babel-preset'],
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+        targets: {
+          node: 'current',
+        },
+      },
+    ],
+    'module:metro-react-native-babel-preset',
+  ],
+  plugins: [
+    // Your existing plugins
+    ['@babel/plugin-proposal-private-property-in-object', { loose: true }],
+  ],
 };
